c72206fbb7967868f97385c0186b577a2857ea27
RMalyk <rmalykzp@gmail.com>
2022-02-21 15:29:06 +0200

    popUp active

--- js/popUP.js
+++ js/popUP.js
@@ -1,45 +1,3 @@
-/**
- * 
- * Работает ==================>
- */
-
-// $body = $('body');
-
-// $('.card__footer-btn').on('click', function (event) {
-
-// 	changeClassPopUp('.popUp__checkout');
-// });
-
-
-// function changeClassPopUp(popUp) {
-// 	let activepopUp = $(popUp);
-// 	console.log(activepopUp	);
-// 	activepopUp.toggleClass('popUp-isActive');
-// }
-
-
-/**
- * 
- * Работает ==================<
- */
-
-// $body = $('body');
-
-// function checkPopUpclick(elem) {
-// 	switch (elem) {
-// 		case 'header__callback-btn':
-// 			return '.popUp__call_back';
-// 		case 'card__footer-btn':
-// 			return '.popUp__checkout';
-// 		default:
-// 			$('.popUp-wrapper').removeClass('popUp-isActive');
-// 			break;
-// 	}
-// }
-
-
-
-// console.log(checkPopUpclick($body));
 
 $body = $('body');
 
@@ -52,57 +10,25 @@ $body.on('click', function (event) {
 		for (let i = 0; i < $classArr.length; i++) {
 			switch ($classArr[i]) {
 				case 'card__footer-btn':
-					console.log(1);
+					changeClassPopUp('.popUp__checkout');
+					console.log(this);
 					break;
 				case 'header__callback-btn':
-					console.log(2);
+					changeClassPopUp('.popUp__call_back');
 					break;
 				case 'btn-order':
-					console.log(2);
+					changeClassPopUp('.popUp__checkout');
+					break;
+				case 'popUp-wrapper':
+					$('.popUp-wrapper').removeClass('popUp-isActive');
 					break;
 
 				default:
 					break;
 			}
-
-
 		}
-
 	}
-
-
-
-
-
-
-
-	// console.log($(event.target).attr('class').split(/\s+/));
-
-	// if ($(event.target).hasClass('card__footer-btn') || $(event.target).hasClass('btn-order')) {
-
-	// 	changeClassPopUp('.popUp__checkout');
-
-	// } else if ($(event.target).hasClass('header__callback-btn')) {
-
-	// 	changeClassPopUp('.popUp__call_back');
-
-	// } else if ($(event.target).hasClass('popUp-wrapper')) {
-
-	// 	$('.popUp-wrapper').removeClass('popUp-isActive');
-
-	// } else {
-	// 	return;
-	// }
 });
-
-
-// $('.card__footer-btn').on('click', function(e) {
-// 	e.preventDefault();
-// 	changeClassPopUp('.popUp__checkout');
-// });
-// $('.popUp-wrapper').click(function name() {
-// 	$('.popUp-wrapper').removeClass('popUp-isActive');
-// });
 
 
 function changeClassPopUp(popUp) {
